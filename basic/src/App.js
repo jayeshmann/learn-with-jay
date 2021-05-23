9a918f3401745aa0f6b717f6f6efe8f350471eeb
Jayesh Mann <jayeshmann06@gmail.com>
2021-05-24 00:11:53 +0530

    Counter App

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,33 +1,17 @@
 import { useState } from 'react';
 
-const Hello = (props) => {
-  console.log(props);
-
-  if (props.age === undefined) {
-    return (
-      <div>
-        <p>Hello {props.name}</p>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old.
-      </p>
-    </div>
-  );
-};
-
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      Value of {count}
+      Counter: {count}
       <br />
       <button onClick={() => setCount(count + 1)}>Increase Count</button>
+      <br />
+      <button onClick={() => setCount(count - 1)}>Decrease Count</button>
+      <br />
+      <button onClick={() => setCount(0)}>Reset Count</button>
     </div>
   );
 };
