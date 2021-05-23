e4cac76ca9218eab7427e021795978d2bb759379
Jayesh Mann <jayeshmann06@gmail.com>
2021-05-24 01:34:19 +0530

    Add styling

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,18 +1,22 @@
 import { useState } from 'react';
+import './App.css';
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      Counter: {count}
+    <main>
+      <h1>Counter: {count}</h1>
+      <br />
       <br />
       <button onClick={() => setCount(count + 1)}>Increase Count</button>
       <br />
+      <br />
       <button onClick={() => setCount(count - 1)}>Decrease Count</button>
       <br />
+      <br />
       <button onClick={() => setCount(0)}>Reset Count</button>
-    </div>
+    </main>
   );
 };
 
